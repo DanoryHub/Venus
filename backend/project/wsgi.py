b9eac2a306b928db844c7d6be7f38d02bc885d95
Ivan Miatselski <Ivan_Miatselski3@epam.com>
2020-05-03 23:55:34 +0300

    create db connection

--- backend/project/wsgi.py
+++ backend/project/wsgi.py
@@ -1,4 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from os import environ
 from app import app
 
+
+def create_db_connection(app):
+    POSTGRES_USER = environ.get("POSTGRES_USER")
+    POSTGRES_PW = environ.get("POSTGRES_PASSWORD")
+    POSTGRES_DB = environ.get("POSTGRES_DB")
+
+    DB_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PW}@localhost:44312/{POSTGRES_DB}'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+
+    SQLAlchemy(app)
+
+
 if __name__ == "__main__":
+    create_db_connection(app)
     app.run()
